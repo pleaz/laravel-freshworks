6e0a303d14cfee97195628a05722553105d97c03
Anton <oprstfaq@gmail.com>
2022-03-30 09:29:14 +0300

    Update Deals.php

--- src/Api/Deals.php
+++ src/Api/Deals.php
@@ -7,6 +7,7 @@ use CodeGreenCreative\Freshworks\Traits\CanBeForgotten;
 use CodeGreenCreative\Freshworks\Traits\CanBulkDestroy;
 use CodeGreenCreative\Freshworks\Traits\CanCloneItself;
 use CodeGreenCreative\Freshworks\Traits\CanPaginate;
+use CodeGreenCreative\Freshworks\Traits\HasFields;
 use CodeGreenCreative\Freshworks\Traits\HasFilters;
 use CodeGreenCreative\Freshworks\Traits\PerformsCrudOperations;
 
